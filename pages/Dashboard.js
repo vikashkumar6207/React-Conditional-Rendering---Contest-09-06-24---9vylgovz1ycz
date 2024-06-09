ac612e9b02e7c5b7f966f7491ecf276281a9fa30
vikashkumar6207 <vktd828403@gmail.com>
2024-06-09 19:01:20 +0530

    complete

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,17 +1,52 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./_app";
 
 
 function Dashboard() {
   const { } = useContext(AuthContext);
+  const [btn1State, setBtn1State] = useState(true);
+  const [btn2State, setBtn2State] = useState(true);
+
+  const handleBtn1Click = () => {
+    setBtn1State(!btn1State);
+  }
+
+  const handleBtn2Click = () => {
+    setBtn2State(!btn2State);
+  }
+
+  useEffect(()=>{
+    
+    if(btn1State && btn2State){
+      document.querySelector('.msg:nth-of-type(1)').style.display = "block";
+
+      document.querySelector('.msg:nth-of-type(2)').style.display = "none";
+
+      document.querySelector('.msg:nth-of-type(3)').style.display = "none";
+    }else if(btn1State || btn2State){
+        document.querySelector('.msg:nth-of-type(1)').style.display = "none";
+
+        document.querySelector('.msg:nth-of-type(2)').style.display = "block";
+
+        document.querySelector('.msg:nth-of-type(3)').style.display = 'none';
+    }else {
+      document.querySelector('.msg:nth-of-type(1)').style.display = "none";
+
+      document.querySelector(".msg:nth-of-type(2)").style.display = "none";
+
+      document.querySelector(".msg:nth-of-type(3)").style.display = "block";
+    }
+  }, [btn1State, btn2State]);
+
+
 
   return (
     <div id="dashboardPage">
-      <button id="btn1" >Button 1</button>
-      <button id="btn2" >Button 2</button>
-      <div className="msg">Both are true</div>
-      <div className="msg">One on them is false</div>
-      <div className="msg">Both are false</div>
+      <button id="btn1" onClick={handleBtn1Click} >Button 1</button>
+      <button id="btn2" onClick={handleBtn2Click}>Button 2</button>
+      <div className="msg" style={{display: 'none'}}>Both are true</div>
+      <div className="msg" style={{display: 'none'}}>One on them is false</div>
+      <div className="msg" style={{display: 'none'}}>Both are false</div>
 
     </div>
   );
